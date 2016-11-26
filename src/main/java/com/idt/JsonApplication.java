1cb57b6fe3b71b97e23c0acf9926f1cbc97eaf88
Eric Santos do Nascimento <ericsantosnascimento@hotmail.com>
2016-11-26 21:15:09 -0200

    improvements

--- src/main/java/com/idt/JsonApplication.java
+++ src/main/java/com/idt/JsonApplication.java
@@ -8,25 +8,14 @@ import java.io.IOException;
 
 /**
  * To parse one json into JsonObject just provide as first arg the json
- * as second arg provide id of the element that you want
+ * as optional second arg provide id of the element to print only the requested object.
+ *
  * <p>
  * Created by eric on 26/11/16.
  */
 public class JsonApplication {
 
     public static void main(String[] args) {
-//        String json = "{\n" +
-//                "\"businesses\": [\n" +
-//                "{\n" +
-//                "\"id\": \"yelp-tropisueno\",\n" +
-//                "\"name\" : \"Tropisueno\",\n" +
-//                "\"display_phone\": \"+1-415-908-3801\",\n" +
-//                "\"image_url\": \"http://s3-media2.ak.yelpcdn.com/bphoto/7DIHu8a0AHhw-BffrDIxPA/ms.jpg\",\n" +
-//                "\"description\": \"\"\n" +
-//                "}\n" +
-//                "]\n" +
-//                "}";
-//        String id = "yelp-tropisueno";
 
         String json = args[0];
         String id = args[1];
@@ -36,15 +25,8 @@ public class JsonApplication {
             ObjectMapper mapper = new ObjectMapper();
             ObjectNode jsonNode = mapper.readValue(json, ObjectNode.class);
             JsonNode foundElement = getElementById(jsonNode, id);
+            printJsonNodeValue(id, jsonNode, foundElement);
 
-            if (id != null) {
-                if (foundElement != null) {
-                    System.out.println(foundElement.toString());
-                }
-
-            } else {
-                System.out.println(jsonNode.toString());
-            }
         } catch (IOException e) {
             System.err.println("Error parsing json");
         }
@@ -53,14 +35,23 @@ public class JsonApplication {
     private static JsonNode getElementById(ObjectNode jsonNode, String id) {
 
         for (JsonNode node : jsonNode.elements().next()) {
-
             if (node.get("id").asText().equals(id)) {
                 return node;
             }
-
         }
-
         return null;
+    }
 
+    private static void printJsonNodeValue(String id, ObjectNode jsonNode, JsonNode foundElement) {
+
+        if (id != null) {
+
+            if (foundElement != null) {
+                System.out.println(foundElement.toString());
+            }
+
+        } else {
+            System.out.println(jsonNode.toString());
+        }
     }
 }
