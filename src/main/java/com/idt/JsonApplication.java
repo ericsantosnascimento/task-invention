72ca6e164b3e6f241308ed1abb0d8343e7f0ffe7
Eric Santos do Nascimento <ericsantosnascimento@hotmail.com>
2016-11-26 22:07:03 -0200

    fixing bug

--- src/main/java/com/idt/JsonApplication.java
+++ src/main/java/com/idt/JsonApplication.java
@@ -7,18 +7,33 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.IOException;
 
 /**
- * To parse one json into JsonObject just provide as first arg the json
+ * Choose which json you want 0 = BUSINESS_JSON OR 1 = SHOPS_JSON
  * as optional second arg provide id of the element to print only the requested object.
- *
+a * <p>
  * <p>
  * Created by eric on 26/11/16.
  */
 public class JsonApplication {
 
+    private static final String BUSINESS_JSON = "{\"businesses\": [{\"id\": \"yelp-tropisueno\",\"name\" : \"Tropisueno\",\"display_phone\": \"+1-415-908-3801\",\"image_url\": \"http://s3-media2.ak.yelpcdn.com/bphoto/7DIHu8a0AHhw-BffrDIxPA/ms.jpg\",\"description\": \"\"}]}";
+
+    private static final String SHOPS_JSON = "{\"shops\": [{\"id\": \"sample-shop\",\"name\" : \"Mingalaba\",\"display_phone\": \"+1-408-366-6600\",\"image_url\": \"http://s3-media2.ak.yelpcdn.com/bphoto/7DIHu8a0AHhw-BffrDIxPA/ms.jpg\",\"description\": \"Best Asian lovingly prepare by Jack \"Papa\" Rasmussen\"}]}";
+
     public static void main(String[] args) {
 
-        String json = args[0];
-        String id = args[1];
+        String json = null;
+        String id = null;
+
+        if (args.length == 0) {
+            System.err.println("No args provided exiting!...");
+            System.exit(1);
+        } else if (args.length > 1){
+            id = args[1];
+        }
+
+        if (args[0].equals("0") || args[0].equals("1")) {
+            json = args[0].equals("0") ? BUSINESS_JSON : SHOPS_JSON;
+        }
 
         try {
 
